43009a35057b63223c319c0f033fb2968c625789
Anna <honey.hanni.k@gmail.com>
2025-04-30 17:03:42 +0300

    corrected task.js

--- task.js
+++ task.js
@@ -1,6 +1,5 @@
 const progress = document.querySelector("#progress");
 const fileInput = document.querySelector("#file");
-const file = fileInput.files[0];
 
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
@@ -9,15 +8,9 @@ document.querySelector("form").addEventListener("submit", (e) => {
 
   xhr.open("POST", " https://students.netoservices.ru/nestjs-backend/upload");
   xhr.upload.onprogress = (e) => {
-    if (e.lenghtComputable) {
-      const percent = e.loaded / e.total;
-      progress.value = percent;
-    }
+    progress.value = e.loaded / e.total;
   };
 
-  xhr.onload = () => {
-    progress.value = 1;
-  };
   const formData = new FormData();
   formData.append("file", file);
   xhr.send(formData);
