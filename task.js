691afc9215d3877596e6c345aa5edf2bf5f21fa7
Anna <honey.hanni.k@gmail.com>
2025-04-30 17:13:45 +0300

    corrected task.js

--- task.js
+++ task.js
@@ -5,13 +5,13 @@ document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
 
   const xhr = new XMLHttpRequest();
-
+  xhr.addEventListener("readystatechange", () => {
+    xhr.upload.onprogress = (e) => {
+      progress.value = e.loaded / e.total;
+    };
+  });
+  
   xhr.open("POST", " https://students.netoservices.ru/nestjs-backend/upload");
-  xhr.upload.onprogress = (e) => {
-    progress.value = e.loaded / e.total;
-  };
-
   const formData = new FormData();
-  formData.append("file", file);
   xhr.send(formData);
 });
